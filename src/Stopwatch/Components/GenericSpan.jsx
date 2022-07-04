90514f39ddaf7317ef94dd0a0483ecfa78a1db3c
Amol Bhandare <anmolhtmldeveloper@gmail.com>
2022-07-04 23:56:07 +0530

    working on stop watch

--- src/Stopwatch/Components/GenericSpan.jsx
+++ src/Stopwatch/Components/GenericSpan.jsx
@@ -3,15 +3,50 @@ import React from "react";
 function GenericSpan(props) {
   switch (props.type) {
     case "start":
-      return <p className="startTimer">{props.text}</p>;
+      return (
+        <p
+          className="startTimer"
+          onClick={props.handleClick ? props.handleClick : () => {}}
+        >
+          {props.text}
+        </p>
+      );
     case "stop":
-      return <p className="stopTimer">{props.text}</p>;
+      return (
+        <p
+          className="stopTimer"
+          onClick={props.handleClick ? props.handleClick : () => {}}
+        >
+          {props.text}
+        </p>
+      );
     case "timer":
-      return <p className="timerValue">{props.text}</p>;
+      return (
+        <p
+          className="timerValue"
+          onClick={props.handleClick ? props.handleClick : () => {}}
+        >
+          {props.text}
+        </p>
+      );
     case "pause":
-      return <p className="pauseTimer">{props.text}</p>;
+      return (
+        <p
+          className="pauseTimer"
+          onClick={props.handleClick ? props.handleClick : () => {}}
+        >
+          {props.text}
+        </p>
+      );
     case "resume":
-      return <p className="resumeTimer">{props.text}</p>;
+      return (
+        <p
+          className="resumeTimer"
+          onClick={props.handleClick ? props.handleClick : () => {}}
+        >
+          {props.text}
+        </p>
+      );
     default:
       return <p className={props.className}>{props.text}</p>;
   }
--- src/Stopwatch/Stopwatch.jsx
+++ src/Stopwatch/Stopwatch.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import GenericSpan from "./Components/GenericSpan";
 
 function Stopwatch() {
+  const [stopWatchState, setStopWatchState] = useState(0);
+
   return (
     <>
       <div className="container">
@@ -11,7 +13,84 @@ function Stopwatch() {
           <p className="timerValue">00:00:00</p>
           <p className="pauseTimer">Pause</p> */}
           {/* <p className="resumeTimer">Resume</p> */}
-          <GenericSpan type="start" text="Start" />
+          {/* <GenericSpan
+            type="start"
+            text="Start"
+            handleClick={() => {
+              console.log("Start");
+            }}
+          /> */}
+
+          {stopWatchState === 0 && (
+            <>
+              <GenericSpan
+                type="start"
+                text="Start"
+                handleClick={() => {
+                  // console.log("Start");
+                  setStopWatchState(1);
+                }}
+              />
+            </>
+          )}
+
+          {stopWatchState === 1 && (
+            <>
+              <GenericSpan
+                type="stop"
+                text="Stop"
+                handleClick={() => {
+                  // console.log("Stop");
+                  setStopWatchState(0);
+                }}
+              />
+              {/* <GenericSpan
+                type="start"
+                text="Start"
+                handleClick={() => {
+                  console.log("Start");
+                }}
+              /> */}
+              <GenericSpan type="timer" text="00:00:00" />
+              <GenericSpan
+                type="pause"
+                text="Pause"
+                handleClick={() => {
+                  // console.log("Pause");
+                  setStopWatchState(2);
+                }}
+              />
+            </>
+          )}
+
+          {stopWatchState === 2 && (
+            <>
+              <GenericSpan
+                type="stop"
+                text="Stop"
+                handleClick={() => {
+                  // console.log("Stop");
+                  setStopWatchState(0);
+                }}
+              />
+              {/* <GenericSpan
+                type="start"
+                text="Start"
+                handleClick={() => {
+                  console.log("Start");
+                }}
+              /> */}
+              <GenericSpan type="timer" text="00:00:00" />
+              <GenericSpan
+                type="resume"
+                text="Resume"
+                handleClick={() => {
+                  // console.log("Resume");
+                  setStopWatchState(1);
+                }}
+              />
+            </>
+          )}
         </div>
       </div>
     </>
